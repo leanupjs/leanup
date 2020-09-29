0bc1eb06bb4c89e75dc1381dc643055124e12fca
Martin Oppitz <github@martinoppitz.com>
2020-09-29 04:05:56 +0200

    refactor: separate framework main files

--- packages/cli/frameworks/vanilla/template/src/main.ts
+++ packages/cli/frameworks/vanilla/template/src/main.ts
@@ -1,15 +1,1 @@
-import './components/app/component.vanilla';
-
-import { DI } from '@leanup/lib/helpers/injector';
-
-DI.register('Framework', {
-  name: 'Vanilla',
-  version: null,
-});
-require('./shares/register');
-require('./shares/routing');
-
-const htmlDivElement: HTMLDivElement | null = document.querySelector('div#vanilla');
-htmlDivElement.style.display = 'inline';
-const appElement: HTMLElement = document.createElement('wc-app');
-htmlDivElement.appendChild(appElement);
+import './main.vanilla';
