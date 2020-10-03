14a8e3577bc310061bac6fba66e4fc4935ea4959
Martin Oppitz <github@martinoppitz.com>
2020-10-04 01:00:21 +0200

    fix: vue format

--- packages/cli/frameworks/vue/template/src/main.vue.ts
+++ packages/cli/frameworks/vue/template/src/main.vue.ts
@@ -9,13 +9,13 @@ import { APP_HTML_ELEMENT } from './shares/constant';
 
 // https://github.com/vuejs/vue-devtools
 const ENVs = {
-  NODE_ENV: '$$NODE_ENV$$'
+  NODE_ENV: '$$NODE_ENV$$',
 };
 Vue.config.devtools = ENVs.NODE_ENV === 'development';
 
 DI.register('Framework', {
   ...require('vue/package.json'),
-  name: 'Vue'
+  name: 'Vue',
 });
 require('./shares/register');
 require('./shares/routing');
@@ -29,6 +29,6 @@ if (htmlDivElement instanceof HTMLDivElement) {
   // tslint:disable-next-line: no-unused-expression
   new Vue({
     el: APP_HTML_ELEMENT,
-    render: (h: CreateElement): VNode => h(App)
+    render: (h: CreateElement): VNode => h(App),
   });
 }
