96135fae48e05cc6d83a92883d024f030732f9b3
Martin Oppitz <github@martinoppitz.com>
2020-11-05 11:08:41 +0100

    fix: copy-modules-webpack-plugin

--- packages/cli/core/src/webpack.config.ts
+++ packages/cli/core/src/webpack.config.ts
@@ -1,5 +1,6 @@
 const path = require('path');
 
+const CopyModulesPlugin = require('copy-modules-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
