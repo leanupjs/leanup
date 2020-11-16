aea09750b6d8017fcdd1d4733659194ab8f4f642
Martin Oppitz <github@martinoppitz.com>
2020-11-16 03:23:35 +0100

    chore: remove more overhead args

--- packages/cli/plugins/addons/webpack.config.js
+++ packages/cli/plugins/addons/webpack.config.js
@@ -1,34 +1,21 @@
 module.exports = (argv, config) => {
-  if (argv.analyzer) {
-    const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-    config.plugins.push(
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        reportFilename: `analyzer.report.html`,
-      })
-    );
-  }
-
-  if (argv.gzip) {
-    const CompressionPlugin = require('compression-webpack-plugin');
-    config.plugins.push(
-      new CompressionPlugin({
-        test: /.(css|js)$/,
-      })
-    );
-  }
-
-  if (argv.worker) {
-    config.module.rules.push({
-      test: /\.worker\.(js|ts)$/i,
-      use: [
-        {
-          loader: 'comlink-loader',
-          options: {
-            singleton: true,
-          },
+  // https://slashgear.github.io/why-you-should-use-compression-webpack-plugin/
+  const CompressionPlugin = require('compression-webpack-plugin');
+  config.plugins.push(
+    new CompressionPlugin({
+      test: /.(css|js)$/,
+    })
+  );
+  // https://github.com/GoogleChromeLabs/comlink-loader
+  config.module.rules.push({
+    test: /\.worker\.(js|ts)$/i,
+    use: [
+      {
+        loader: 'comlink-loader',
+        options: {
+          singleton: true,
         },
-      ],
-    });
-  }
+      },
+    ],
+  });
 };
