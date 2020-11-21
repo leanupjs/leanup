b7d60ca8a63547cc36dbb59c8c3c6e7dd77ae701
Martin Oppitz <martinoppitz@users.noreply.github.com>
2020-11-21 20:28:42 +0100

    Update wake-up.sh

--- scripts/wake-up.sh
+++ scripts/wake-up.sh
@@ -2,22 +2,15 @@ rm -rf workspace || { echo 'command failed' ; exit 1; }
 mkdir workspace || { echo 'command failed' ; exit 1; }
 cd workspace || { echo 'command failed' ; exit 1; }
 npm install @leanup/cli@$2 @leanup/cli-$1@$2 --no-save || { echo 'command failed' ; exit 1; }
-# npx lean create -o -n template || { echo 'command failed' ; exit 1; }
 npx $1 create -o -n template || { echo 'command failed' ; exit 1; }
+npm audit || { echo 'command failed' ; exit 1; }
 npm update || { echo 'command failed' ; exit 1; }
-# npx lean lint || { echo 'command failed' ; exit 1; }
 npx $1 lint || { echo 'command failed' ; exit 1; }
-# npx lean format || { echo 'command failed' ; exit 1; }
 npx $1 format || { echo 'command failed' ; exit 1; }
-# npx lean test || { echo 'command failed' ; exit 1; }
 # npx $1 test || { echo 'command failed' ; exit 1; }
-# npx lean coverage || { echo 'command failed' ; exit 1; }
 npx $1 coverage || { echo 'command failed' ; exit 1; }
-# npx lean build --gzip || { echo 'command failed' ; exit 1; }
 npx $1 build || { echo 'command failed' ; exit 1; }
-# npx lean serve -o -m production || { echo 'command failed' ; exit 1; }
 # npx $1 serve -o -m production || { echo 'command failed' ; exit 1; }
-# npx lean e2e -e chrome || { echo 'command failed' ; exit 1; }
 # npx $1 e2e -e chrome || { echo 'command failed' ; exit 1; }
 npm outdated || true
 npm list --depth=0 || true
