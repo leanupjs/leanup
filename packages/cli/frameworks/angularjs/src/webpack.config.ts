8bf21beb1592202aada23087d119e4002eba1dc6
Martin Oppitz <github@martinoppitz.com>
2020-11-22 20:31:52 +0100

    fix: angularjs and inferno webpack.config

--- packages/cli/frameworks/angularjs/src/webpack.config.ts
+++ packages/cli/frameworks/angularjs/src/webpack.config.ts
@@ -1,4 +1,4 @@
-import { BABEL_LOADER } from '@leanup/cli-core-babel/webpack.config';
+import { BABEL_LOADER } from '@leanup/cli-core-babel/lib/webpack.config';
 import { webpackConfig } from '@leanup/cli/lib/webpack.config';
 
 export function webpackAngularJSConfig(env: any, argv: any): Object {
