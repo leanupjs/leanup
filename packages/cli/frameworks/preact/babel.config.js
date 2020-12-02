7b69d3a328b3296c7e116ee23b71d8fbf5c09051
Martin Oppitz <github@martinoppitz.com>
2020-12-02 11:17:14 +0100

    fix: preact babel config in v1.0

--- packages/cli/frameworks/preact/babel.config.js
+++ packages/cli/frameworks/preact/babel.config.js
@@ -4,7 +4,17 @@ config.plugins.push([
   '@babel/plugin-transform-react-jsx',
   {
     pragma: 'h',
-    pragmaFrag: 'Fragment',
+  },
+]);
+
+const index = config.presets.indexOf('@babel/preset-typescript');
+if (index >= 0) {
+  config.presets.splice(index, 1);
+}
+config.presets.push([
+  '@babel/preset-typescript',
+  {
+    jsxPragma: 'h',
   },
 ]);
 
