05056d03289552c8157a8d7af4c66f3462091584
Martin Oppitz <github@martinoppitz.com>
2020-12-13 01:34:54 +0100

    chore: rename files

---
+++ packages/cli/frameworks/lit-element/template/src/components/app/component.lit-element.ts
@@ -0,0 +1,38 @@
+import '../series/list/component.lit-element';
+
+import { customElement, html, TemplateResult } from 'lit-element';
+
+import { RouterService } from '../../services/router/service';
+import { LitElementComponent } from '../component.lit-element';
+import { AppController, ResolvedRoute } from './controller';
+
+@customElement('app-component')
+export class AppComponent extends LitElementComponent {
+  private readonly ctrl: AppController = new AppController();
+  private resolvedRoute: ResolvedRoute = {
+    url: 'series',
+  };
+
+  public constructor() {
+    super();
+    RouterService.subscribe((route: { url: string }, params: { id: number }, query: unknown) => {
+      this.resolvedRoute = {
+        params,
+        query,
+        url: route.url,
+      };
+      this.update(new Map());
+    });
+  }
+
+  public render(): TemplateResult {
+    return html`<div id="app">
+      <code>- is currently not full implemented -</code>
+      <h4>${this.ctrl.framework.name} v${this.ctrl.framework.version}</h4>
+      <small>${this.ctrl.finishedRendering} ms upcomming time</small>
+      ${this.resolvedRoute.url === 'series' ? html`<list-serie-component />` : ''}
+      <br />
+      <small> Build with: ${this.ctrl.cli.name} v${this.ctrl.cli.version} </small>
+    </div>`;
+  }
+}
