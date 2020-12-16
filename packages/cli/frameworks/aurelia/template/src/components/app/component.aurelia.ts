c67ca45fdb4b58b1e565264ec1b0fc4460efb65e
Martin Oppitz <github@martinoppitz.com>
2020-12-17 00:04:37 +0100

    fix: the aurelia image handling

--- packages/cli/frameworks/aurelia/template/src/components/app/component.aurelia.ts
+++ packages/cli/frameworks/aurelia/template/src/components/app/component.aurelia.ts
@@ -1,11 +1,9 @@
 import IMG_FRAMEWORK from '../../assets/logo.aurelia.png';
-import IMG_LEANUP from '../../assets/logo.leanupjs.png';
 import { RouterService } from '../../services/router/service';
-import { Filters } from '../../shares/filters';
 import { AppController, ResolvedRoute } from './controller';
 
 export class App extends AppController {
-  public filter: Filters = Filters;
+  public readonly frameworkImg: string = IMG_FRAMEWORK as string;
   public resolvedRoute: ResolvedRoute = {
     url: 'series',
   };
