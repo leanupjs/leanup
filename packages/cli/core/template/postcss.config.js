26c3f2be176bc8d4d685cf07e22eb3a2dbba6d0f
Martin Oppitz <github@martinoppitz.com>
2020-12-16 14:09:52 +0100

    chore: optimize templates

--- packages/cli/core/template/postcss.config.js
+++ packages/cli/core/template/postcss.config.js
@@ -1,9 +1,10 @@
 const config = require('@leanup/stack/postcss.config');
 
-config.plugins = [require('tailwindcss')].concat([require('autoprefixer')]).concat([
+config.plugins.unshift(require('tailwindcss'));
+config.plugins.push(
   require('cssnano')({
     preset: 'default',
-  }),
-]);
+  })
+);
 
 module.exports = config;
