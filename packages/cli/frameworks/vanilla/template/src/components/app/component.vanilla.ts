5e4920eee3552fc8f295286cbe15a4ba15d86343
Martin Oppitz <github@martinoppitz.com>
2020-12-17 01:10:29 +0100

    chore: tiny styling

--- packages/cli/frameworks/vanilla/template/src/components/app/component.vanilla.ts
+++ packages/cli/frameworks/vanilla/template/src/components/app/component.vanilla.ts
@@ -32,7 +32,7 @@ function render(component: AppComponent) {
   <small>Used filters: ${Filters.date(component.$ctrl.dummies.date)} | ${Filters.currency(
     component.$ctrl.dummies.price
   )} €</small><br />
-  <small>Build with: ${component.$ctrl.cli.name} v${component.$ctrl.cli.version}}</small>
+  <small>Build with: ${component.$ctrl.cli.name} v${component.$ctrl.cli.version}}</small><br />
   <code>- is currently not full implemented -</code>
 </div>`;
   return html;
