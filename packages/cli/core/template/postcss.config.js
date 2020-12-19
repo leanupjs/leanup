f705ca46d4b6c6f698925ecfc9a63bcbcebde92a
Martin Oppitz <github@martinoppitz.com>
2020-12-19 14:33:41 +0100

    chore: update to new template design

--- packages/cli/core/template/postcss.config.js
+++ packages/cli/core/template/postcss.config.js
@@ -1,1 +1,10 @@
-module.exports = require('@leanup/cli/postcss.config');
+const config = require('@leanup/cli/postcss.config');
+
+config.plugins.unshift();
+config.plugins.push(
+  require('cssnano')({
+    preset: 'default',
+  })
+);
+
+module.exports = config;
---
+++ packages/cli/core/template/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.svelte', './src/**/*.ts', './src/**/*.tsx', './src/**/*.vue'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      tableLayout: ['hover', 'focus'],
+    },
+  },
+  plugins: [],
+};
