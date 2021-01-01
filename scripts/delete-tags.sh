adb0126e4d70f8906f85e73df4a28c9da3f00c6b
Martin Oppitz <github@martinoppitz.com>
2021-01-01 05:05:44 +0100

    chore: script to delete tags

---
+++ scripts/delete-tags.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+git tag -l | grep v1.
+git tag -l | grep alpha
+git tag -l | grep beta
+git tag -l | grep webpack
+# for i in {0..80}
+# do
+#    git tag --delete "v1.0.$i"
+#    git push --delete origin "v1.0.$i"
+# done
+
+# git tag -l | grep v1.1.0-rc.
+# for i in {240..300}
+# do
+#    git tag --delete "v1.1.0-rc.$i"
+#    git push --delete origin "v1.1.0-rc.$i"
+# done
+
+# git tag -l | grep 1.0.0-beta.
+# for i in {0..8}
+# do
+#    git tag --delete "1.0.0-beta.$i"
+#    git push --delete origin "1.0.0-beta.$i"
+#    git tag --delete "v1.0.0-beta.$i"
+#    git push --delete origin "v1.0.0-beta.$i"
+# done
+
+# git tag -l | grep v1.0.0-rc.
+# for i in {501..509}
+# do
+#    git tag --delete "v1.0.0-rc.$i"
+#    git push --delete origin "v1.0.0-rc.$i"
+# done
+
+# git tag --delete "v1.0.60-alpha.0"
+# git push --delete origin "v1.0.60-alpha.0"
