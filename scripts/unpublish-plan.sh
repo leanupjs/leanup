a1a95c27590ba63b057e1c6cd3a16e9bfa8fc5ac
Martin Oppitz <github@martinoppitz.com>
2021-01-08 05:43:10 +0100

    chore: edit unpublish scripts

--- scripts/unpublish-plan.sh
+++ scripts/unpublish-plan.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 for (( i=$(($2)); i<=$(($3)); i++ ))
 do
    sh unpublish-all.sh "$1.$i"
--- scripts/unpublish.sh
+++ scripts/unpublish.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 echo
+echo args: $1, $2, $3, $4
+# echo "sum: $(($3 + $4))"
+
+echo
 echo npm access restricted @leanup/$1
 npm access restricted @leanup/$1
 
-for (( i=$(($3)); i<=$(($4)); i++ ))
+for i in {0..1}
+# for (( i=0; i<=0; i++ ))
 do
     echo npm unpublish "@leanup/$1@$2.$i" --force
     npm unpublish "@leanup/$1@$2.$i" --force
