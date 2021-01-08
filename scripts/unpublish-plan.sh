8cfbae86191b6892c0c44ffdbb96ead8b6239c77
Martin Oppitz <github@martinoppitz.com>
2021-01-08 05:29:50 +0100

    chore: add unpublish logic

---
+++ scripts/unpublish-plan.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for (( i=$(($2)); i<=$(($3)); i++ ))
+do
+   sh unpublish-all.sh "$1.$i"
+done
--- scripts/unpublish.sh
+++ scripts/unpublish.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
 echo
-echo npm unpublish @leanup/$1@$2 -f
-npm unpublish @leanup/$1@$2 -f
+echo npm access restricted @leanup/$1
+npm access restricted @leanup/$1
+
+for (( i=$(($3)); i<=$(($4)); i++ ))
+do
+    echo npm unpublish "@leanup/$1@$2.$i" --force
+    npm unpublish "@leanup/$1@$2.$i" --force
+done
+
+echo npm access public @leanup/$1
+npm access public @leanup/$1
