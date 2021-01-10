8e1516848fd1fa4826dd07cfa154521acdb71379
Martin Oppitz <github@martinoppitz.com>
2021-01-10 13:48:55 +0100

    fix: framework tests

--- scripts/wake-up.sh
+++ scripts/wake-up.sh
@@ -1,34 +1,30 @@
-echo rm -rf workspace
 rm -rf workspace
-echo mkdir workspace
 mkdir workspace
-echo cd workspace
 cd workspace
-echo "npm install --no-save @leanup/cli@$2 @leanup/cli-$1@$2"
-npm install --no-save "@leanup/cli@$2" "@leanup/cli-$1@$2"
-# npx lean create -n template -o
-# npx $1 create -n template -o
-# npx lean lint
-# npx $1 lint
-# npm run lint
-# npx lean format
-# npx $1 format
-# npm run format
-# npx lean test
-# npx $1 test
-# npm run test
-# npx lean coverage
-# npx $1 coverage
-# npm run coverage
-# npx lean build
-# npx $1 build
-# npm run build
-# # npx lean serve --port 8080 &
-# # npx $1 serve --port 8081 &
-# # npx lean serve -- --port 8082 &
-# # npx lean e2e -e chrome
-# # npx $1 e2e -e chrome
-# npm outdated || true
-# npm list --depth=0 || true
-# cd ..
-# rm -rf workspace
+npm install --no-save "@leanup/cli@$2" "@leanup/cli-$1@$2" || { echo 'command failed' ; exit 1; }
+npx lean create -n template -o || { echo 'command failed' ; exit 1; }
+npx $1 create -n template -o || { echo 'command failed' ; exit 1; }
+npx lean lint || { echo 'command failed' ; exit 1; }
+npx $1 lint || { echo 'command failed' ; exit 1; }
+npm run lint || { echo 'command failed' ; exit 1; }
+npx lean format || { echo 'command failed' ; exit 1; }
+npx $1 format || { echo 'command failed' ; exit 1; }
+npm run format || { echo 'command failed' ; exit 1; }
+npx lean test || { echo 'command failed' ; exit 1; }
+npx $1 test || { echo 'command failed' ; exit 1; }
+npm run test || { echo 'command failed' ; exit 1; }
+npx lean coverage || { echo 'command failed' ; exit 1; }
+npx $1 coverage || { echo 'command failed' ; exit 1; }
+npm run coverage || { echo 'command failed' ; exit 1; }
+npx lean build || { echo 'command failed' ; exit 1; }
+npx $1 build || { echo 'command failed' ; exit 1; }
+npm run build || { echo 'command failed' ; exit 1; }
+# npx lean serve --port 8080 &
+# npx $1 serve --port 8081 &
+# npx lean serve -- --port 8082 &
+# npx lean e2e -e chrome
+# npx $1 e2e -e chrome
+npm outdated || true
+npm list --depth=0 || true
+cd ..
+rm -rf workspace
