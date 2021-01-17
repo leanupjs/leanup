a32fc73ebbff2ec1455a8a923874a30a3940d9e2
Martin Oppitz <github@martinoppitz.com>
2021-01-17 05:48:41 +0100

    chore: add new cleanup workflow

--- scripts/unpublish-one.sh
+++ scripts/unpublish-one.sh
@@ -30,14 +30,14 @@ do
     npm unpublish --force "@leanup/$1@$version.$i"
 done
 
-version="1.1.0-rc"
-for (( i=162; i<=230; i++ ))
-do
-    bash ./deprecate.sh "$1" "$version.$i" "The version $version.$i of the package @leanup/$1 is no longer maintained and will be removed from the registry shortly. Instead, please switch to a newer, well-maintained version."
+# version="1.1.0-rc"
+# for (( i=162; i<=230; i++ ))
+# do
+#     bash ./deprecate.sh "$1" "$version.$i" "The version $version.$i of the package @leanup/$1 is no longer maintained and will be removed from the registry shortly. Instead, please switch to a newer, well-maintained version."
 
-    echo
-    echo npm unpublish --force "@leanup/$1@$version.$i"
-    npm unpublish --force "@leanup/$1@$version.$i"
-done
+#     echo
+#     echo npm unpublish --force "@leanup/$1@$version.$i"
+#     npm unpublish --force "@leanup/$1@$version.$i"
+# done
 
 bash ./access.sh public $1
