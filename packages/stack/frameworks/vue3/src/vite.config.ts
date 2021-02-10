a5f63f8738877c924a87f2c30b435a4543b43bb6
Martin Oppitz <github@martinoppitz.com>
2021-02-10 08:07:43 +0100

    fix: remove redundant vite config

--- packages/stack/frameworks/vue3/src/vite.config.ts
+++
@@ -1,7 +0,0 @@
-const config = require('@leanup/stack/vite.config');
-const vue = require('@vitejs/plugin-vue');
-
-config.plugins = config.plugins || [];
-config.plugins.push(vue());
-
-module.exports = config;
