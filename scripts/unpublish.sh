35d1964a2ee7acdfde1dd30cf5fcfa3b0eb789c5
Martin Oppitz <github@martinoppitz.com>
2021-02-13 04:50:21 +0100

    chore: working on scripts

--- scripts/unpublish.sh
+++ scripts/unpublish.sh
@@ -1,6 +1,7 @@
 bash ./access.sh restricted $1
 
 bash ./deprecate.sh "$1" "$2" "The version $2 of the package @leanup/$1 is no longer maintained and will be removed from the registry shortly. Instead, please switch to a newer, well-maintained version."
+# bash ./deprecate.sh "$1" "$2" "This version $2 of the package @leanup/$1 contains a bug and is marked as deprecated. Instead, please switch to a newer, well-maintained version."
 
 echo
 echo npm unpublish --force "@leanup/$1@$2"
