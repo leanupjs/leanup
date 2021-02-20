1ff2c9ede6f47af65684e8aa1f614944e6585b31
Martin Oppitz <github@martinoppitz.com>
2021-02-20 03:11:45 +0100

    chore: sync template to @leanup/cli-angular@v1.1

--- packages/cli/frameworks/angular/template/src/angular.main.ts
+++ packages/cli/frameworks/angular/template/src/angular.main.ts
@@ -3,9 +3,15 @@ import 'core-js/features/reflect';
 import 'zone.js/dist/zone';
 
 import { enableProdMode } from '@angular/core';
+import * as PACKAGE_JSON from '@angular/core/package.json';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './angular.module';
+import { run } from './app.run';
+import { APP_HTML_ELEMENT } from './shares/constant';
+import { typeIt } from './shares/utils';
+
+const TYPED_PACKAGE_JSON = typeIt<{ version: string }>(PACKAGE_JSON);
 
 const ENVs = {
   NODE_ENV: '$$NODE_ENV$$',
@@ -14,8 +20,15 @@ if (ENVs.NODE_ENV === 'production') {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .then(() => {})
-  .catch(() => {})
-  .finally(() => {});
+run('Angular', TYPED_PACKAGE_JSON.version, () => {
+  const htmlDivElement: HTMLDivElement | null = document.querySelector('div#angular');
+  if (htmlDivElement instanceof HTMLDivElement) {
+    htmlDivElement.appendChild(APP_HTML_ELEMENT);
+
+    platformBrowserDynamic()
+      .bootstrapModule(AppModule)
+      .then(() => {})
+      .catch(() => {})
+      .finally(() => {});
+  }
+});
