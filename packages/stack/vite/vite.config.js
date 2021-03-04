cebdb3b75768775fa1aba50e1af3c641045ee41c
Martin Oppitz <github@martinoppitz.com>
2021-03-04 23:11:49 +0100

    chore: add css preprocessorOptions for less and sass / vite

--- packages/stack/vite/vite.config.js
+++ packages/stack/vite/vite.config.js
@@ -3,6 +3,17 @@ const { resolve } = require('path');
 // https://vitejs.dev/config/
 module.exports = {
   css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+      sass: {
+        implementation: require('sass'),
+        sassOptions: {
+          includePaths: ['node_modules'],
+        },
+      },
+    },
     postcss: require(resolve(process.cwd(), 'postcss.config')),
   },
   build: {
