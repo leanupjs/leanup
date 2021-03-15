5e39930ba177051b5ca2817ac32f51492aee8aab
Martin Oppitz <github@martinoppitz.com>
2021-03-15 16:17:02 +0100

    fix: the usage of jsxInject

--- packages/stack/frameworks/preact/vite.config.js
+++ packages/stack/frameworks/preact/vite.config.js
@@ -1,6 +1,7 @@
-const config = require('@leanup/stack-vite/vite.config');
+let config = require('@leanup/stack-vite/vite.config');
 const withPreact = require('@preact/preset-vite');
-
-module.exports = withPreact(config, {
+config = withPreact.default(config, {
   // devtoolsInProd: true
 });
+delete config.esbuild.jsxInject;
+module.exports = config;
