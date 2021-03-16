0a6992ef8e4d65e247801c0fa246c51838cf9cf3
Martin Oppitz <github@martinoppitz.com>
2021-03-17 00:20:21 +0100

    chore: configure tailwindcss jit

--- packages/cli/core/template/postcss.config.js
+++ packages/cli/core/template/postcss.config.js
@@ -1,6 +1,6 @@
 const config = require('@leanup/stack/postcss.config');
 
-config.plugins.unshift(require('tailwindcss'));
+config.plugins.unshift(require('@tailwindcss/jit'));
 config.plugins.push(
   require('cssnano')({
     preset: 'default',
