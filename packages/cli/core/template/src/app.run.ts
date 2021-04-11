56b8b9a7a30e1d409f17802c67879457cfeff3f6
Martin Oppitz <github@martinoppitz.com>
2021-04-12 01:02:39 +0200

    chore: store windi magic

--- packages/cli/core/template/src/app.run.ts
+++ packages/cli/core/template/src/app.run.ts
@@ -27,3 +27,23 @@ export const run = (name: string, version: string | null, bootstrap: Function): 
     })
     .catch(importCatch);
 };
+
+const regexClassGroup = /([!\w+-][\w+:_/-]*?\w):\(([\w\s/-]*?)\)/gm;
+function transformGroups(str: string) {
+  return str.replace(regexClassGroup, (_, a: string, b: string) =>
+    b
+      .split(/\s/g)
+      .map((i) => `${a}:${i}`)
+      .join(' ')
+  );
+}
+
+document.addEventListener('DOMNodeInserted', (event: MutationEvent) => {
+  const nodes: NodeListOf<HTMLElement> = event.relatedNode.querySelectorAll('[class]');
+  nodes.forEach((node: HTMLElement) => {
+    const transformed = transformGroups(node.getAttribute('class') || '');
+    if (node.getAttribute('class') != transformed) {
+      node.setAttribute('class', transformed);
+    }
+  });
+});
