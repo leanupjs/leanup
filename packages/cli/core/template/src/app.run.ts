ba2e963a508fa3825a8d673a6d0e8833a4b36feb
Martin Oppitz <github@martinoppitz.com>
2021-04-16 00:23:31 +0200

    chore: remove wrong code

--- packages/cli/core/template/src/app.run.ts
+++ packages/cli/core/template/src/app.run.ts
@@ -1,5 +1,4 @@
 import { DI } from '@leanup/lib/helpers/injector';
-
 export const importCatch = (error: unknown): void => {
   console.warn(error);
 };
@@ -27,23 +26,3 @@ export const run = (name: string, version: string | null, bootstrap: Function): 
     })
     .catch(importCatch);
 };
-
-const regexClassGroup = /([!\w+-][\w+:_/-]*?\w):\(([\w\s/-]*?)\)/gm;
-function transformGroups(str: string) {
-  return str.replace(regexClassGroup, (_, a: string, b: string) =>
-    b
-      .split(/\s/g)
-      .map((i) => `${a}:${i}`)
-      .join(' ')
-  );
-}
-
-document.addEventListener('DOMNodeInserted', (event: MutationEvent) => {
-  const nodes: NodeListOf<HTMLElement> = event.relatedNode.querySelectorAll('[class]');
-  nodes.forEach((node: HTMLElement) => {
-    const transformed = transformGroups(node.getAttribute('class') || '');
-    if (node.getAttribute('class') != transformed) {
-      node.setAttribute('class', transformed);
-    }
-  });
-});
