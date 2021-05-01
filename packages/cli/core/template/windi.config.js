97822627b665e73ec30fb3252e82a0ba9ab4053c
Martin Oppitz <github@martinoppitz.com>
2021-05-01 07:37:31 +0200

    chore: add dark mode to windicss

--- packages/cli/core/template/windi.config.js
+++ packages/cli/core/template/windi.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'windicss/helpers';
 
 export default defineConfig({
+  darkMode: 'media',
   extract: {
     include: ['src/**/*.{html,svelte,ts,tsx,vue}'],
   },
+  plugins: [require('windicss/plugin/filters')],
 });
