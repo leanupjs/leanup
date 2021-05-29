f6062c06a9fc1bfee2c220dcd5026601c7110365
Martin Oppitz <github@martinoppitz.com>
2021-05-29 02:12:51 +0200

    chore: add postcss advice

--- packages/stack/core/postcss.config.js
+++ packages/stack/core/postcss.config.js
@@ -1,3 +1,7 @@
+console.log(
+  `ADVICE: The config file 'postcss.config.js' and the 'autoprefixer' dependency will be removed from '@leanup/stack' package in the next release v1.2. Please make your 'postcss.config.js' independend from us.`
+);
+
 module.exports = {
   plugins: [require('autoprefixer')],
 };
