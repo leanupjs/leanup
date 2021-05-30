22c4314517f7e2b384eb0a21fa28e7cf586bdd92
Martin Oppitz <github@martinoppitz.com>
2021-05-31 01:06:30 +0200

    chore: remove build artifacts

--- packages/form/handlers/validation.handler.d.ts
+++
@@ -1,5 +0,0 @@
-import { ListOf } from '@leanup/lib/pattern/list-of';
-export declare class ValidationHandler {
-    readonly validators: ListOf<Function>;
-    validate(value: unknown, fast?: boolean): Error[];
-}
