40c2ec2ed1a414c4ce36768b4a928acb0e530dbe
Martin Oppitz <github@martinoppitz.com>
2021-07-13 04:43:22 +0200

    refactor: central logger

--- packages/lib/src/helpers/log.ts
+++ packages/lib/src/helpers/log.ts
@@ -1,15 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import * as log from 'loglevel';
+import * as loglevel from 'loglevel';
 
 import { Validator } from './validator';
 
 const LOG_CACHE: Object[] = [];
 
-/**
- * Tiny hack
- */
 function getEnvironment(): string {
-  return '$$NODE_ENV$$';
+  return process.env.NODE_ENV;
 }
 
 interface LogMessage {
@@ -18,12 +15,39 @@ interface LogMessage {
   refObject?: unknown;
 }
 
-export class Log {
-  private static _instance: Log = null;
+export interface LoggerOptions {
+  /**
+   * CSS style of the console log shield.
+   */
+  shieldStyle?: string;
+}
 
-  private constructor() {}
+export class Logger {
+  private static readonly instances: Map<string, Logger> = new Map<string, Logger>();
 
-  private static log(level: string, message: LogMessage): void {
+  private readonly shield: string[];
+
+  private constructor(identifier: string, options: LoggerOptions = {}) {
+    this.shield = [
+      `%c${identifier}`,
+      Validator.isString(options.shieldStyle, 1)
+        ? options.shieldStyle
+        : `color: white; background: #666; font-weight: bold; font-size: 10px; padding: 2px 6px; border-radius: 3px; border: 1px solid #000`,
+    ];
+  }
+
+  public static getInstance(identifier: string, options: LoggerOptions = {}) {
+    if (Validator.isString(identifier, 1)) {
+      if (Logger.instances.has(identifier) === false) {
+        Logger.instances.set(identifier, new Logger(identifier));
+      }
+      return Logger.instances.get(identifier);
+    } else {
+      throw new Error(`The identifier of the Logger must be a string with a min length of 1.`);
+    }
+  }
+
+  private log(level: string, message: LogMessage): void {
     if (
       Validator.isObject(message.refObject) &&
       typeof message.refObject.constructor === 'function' &&
@@ -41,42 +65,43 @@ export class Log {
 
     switch (level) {
       case 'trace':
-        log.trace(message);
+        loglevel.trace(...this.shield, message);
         break;
       case 'debug':
-        log.debug(message);
+        loglevel.debug(...this.shield, message);
         break;
       case 'info':
-        log.info(message);
+        loglevel.info(...this.shield, message);
         break;
       case 'warn':
-        log.warn(message);
+        loglevel.warn(...this.shield, message);
         break;
       case 'error':
-        log.error(message);
-    }
-    if (getEnvironment() === 'development' && level === 'error') {
-      throw new Error(`Execution in development mode was canceled. See the error log above.`);
+      default:
+        loglevel.error(...this.shield, message);
+        if (getEnvironment() === 'development' && level === 'error') {
+          throw new Error(`↑ Execution in development mode was canceled. See the error log above.`);
+        }
     }
   }
 
-  public static trace(messageText: string, refObject?: Object): Log {
+  public trace(messageText: string, refObject?: Object): Logger {
     this.log('trace', { messageText, refObject });
     return this;
   }
-  public static debug(messageText: string, refObject?: Object): Log {
+  public debug(messageText: string, refObject?: Object): Logger {
     this.log('debug', { messageText, refObject });
     return this;
   }
-  public static info(messageText: string, refObject?: Object): Log {
+  public info(messageText: string, refObject?: Object): Logger {
     this.log('info', { messageText, refObject });
     return this;
   }
-  public static warn(messageText: string, refObject?: Object): Log {
+  public warn(messageText: string, refObject?: Object): Logger {
     this.log('warn', { messageText, refObject });
     return this;
   }
-  public static error(messageText: string, refObject?: Object): Log {
+  public error(messageText: string, refObject?: Object): Logger {
     this.log('error', { messageText, refObject });
     return this;
   }
@@ -88,11 +113,14 @@ export class Log {
 
 switch (getEnvironment()) {
   case 'development':
-    log.setDefaultLevel('trace');
+    loglevel.setDefaultLevel('trace');
     break;
   case 'test':
-    log.setDefaultLevel('warn');
+    loglevel.setDefaultLevel('warn');
     break;
   case 'production':
-    log.setDefaultLevel('error');
+  default:
+    loglevel.setDefaultLevel('error');
 }
+
+export const Log: Logger = Logger.getInstance('leanup');
