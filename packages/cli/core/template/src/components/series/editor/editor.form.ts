55127c106757abc2d0ce16d420c72ba665d46598
Martin Oppitz <github@martinoppitz.com>
2021-08-12 05:25:09 +0200

    fix: naming getControl

--- packages/cli/core/template/src/components/series/editor/editor.form.ts
+++ packages/cli/core/template/src/components/series/editor/editor.form.ts
@@ -3,13 +3,13 @@ import { FormControl, InputControl } from '@leanup/form';
 export class EditorForm extends FormControl {
   public constructor(name: string) {
     super(name);
-    this.addConrol(
+    this.addControl(
       new InputControl('title', {
         label: 'Title',
         value: '',
       })
     );
-    this.addConrol(
+    this.addControl(
       new InputControl('unit', {
         label: 'Unit',
         value: '',
