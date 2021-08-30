507165b3c49581aa7ea14a2c73421931c7f1661d
Martin Oppitz <github@martinoppitz.com>
2021-08-31 00:58:04 +0200

    chore: integrate openapi demo in templates

--- packages/cli/core/template/src/components/app/controller.ts
+++ packages/cli/core/template/src/components/app/controller.ts
@@ -1,7 +1,6 @@
 import { AbstractController, DI, ViewControllerCouple } from '@leanup/lib';
+import IMG_LEANUP, { IVersion, VersionApi } from '@leanup/openapi-demo';
 
-import IMG_LEANUP from '../../assets/logo.leanupjs.png';
-import { IVersion, VersionApi } from '../../assets/openapi/typescript-rxjs';
 import { Framework } from '../../models/framework.interface';
 import { RouterService } from '../../services/router/service';
 import { CLI_DETAILS, STARTUP_TIMESTAMP } from '../../shares/constant';
--- packages/cli/core/template/src/shares/register.ts
+++ packages/cli/core/template/src/shares/register.ts
@@ -1,6 +1,6 @@
 import { DI } from '@leanup/lib';
+import { VersionApi } from '@leanup/openapi-demo';
 
-import { VersionApi } from '../assets/openapi/typescript-rxjs';
 import { MeasurementService } from '../services/measurements/service';
 import { RouterService } from '../services/router/service';
 import { StorageService } from '../services/storage/service';
