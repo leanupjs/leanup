7eac4e82630756ae152b1593ec9fb75330f53a43
Martin Oppitz <github@martinoppitz.com>
2021-10-30 11:23:02 +0200

    chore: remove class table

--- packages/cli/frameworks/inferno/template/src/components/series/list/component.inferno.tsx
+++ packages/cli/frameworks/inferno/template/src/components/series/list/component.inferno.tsx
@@ -21,7 +21,7 @@ export class ListSerieComponent extends Component<unknown, unknown> implements G
     const tables: any[] = [];
     for (let i = 0; i < this.ctrl.elements.length; i++) {
       tables.push(
-        <table className="table" key={i}>
+        <table key={i}>
           <thead>
             <tr>
               <th scope="col">#</th>
