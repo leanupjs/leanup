166f08eea7e603e21eecfe73ebde778075fd12e3
Martin Oppitz <github@martinoppitz.com>
2021-11-04 05:29:37 +0100

    chore: add angular babel linker plugin

--- packages/stack/frameworks/angular/src/webpack.config.ts
+++ packages/stack/frameworks/angular/src/webpack.config.ts
@@ -1,4 +1,17 @@
 import { LEANUP_WEBPACK_RULES, webpackConfig } from '@leanup/stack-webpack/lib/webpack.config';
+// import linkerPlugin from '@angular/compiler-cli/linker/babel';
+
+const LINKER_PLUGIN = {
+  test: /\.m?js$/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      // plugins: [linkerPlugin],
+      compact: false,
+      cacheDirectory: true,
+    },
+  },
+};
 
 function removeLoader(config: any, loader: any) {
   const index = config.module.rules.indexOf(loader);
@@ -8,13 +21,13 @@ function removeLoader(config: any, loader: any) {
 }
 
 export function webpackAngularConfig(env: any, argv: any): Object {
-  const config: any = webpackConfig(env, argv);
+  const config: any = webpackConfig(env, argv, [LINKER_PLUGIN]);
   const path = require('path');
 
   const { AngularWebpackPlugin } = require('@ngtools/webpack');
 
-  removeLoader(config,  LEANUP_WEBPACK_RULES.ESBUILD_LOADER_TS);
-  removeLoader(config,  LEANUP_WEBPACK_RULES.SWC_LOADER_TS);
+  removeLoader(config, LEANUP_WEBPACK_RULES.ESBUILD_LOADER_TS);
+  removeLoader(config, LEANUP_WEBPACK_RULES.SWC_LOADER_TS);
 
   config.module.rules.push({
     test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
