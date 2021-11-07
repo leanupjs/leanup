8c34e920304ae1afdd7d6cb7df7abc9e881af8a9
Martin Oppitz <github@martinoppitz.com>
2021-11-07 14:00:47 +0100

    format: fix

--- packages/cli/frameworks/aurelia/template/src/components/input/component.aurelia.ts
+++ packages/cli/frameworks/aurelia/template/src/components/input/component.aurelia.ts
@@ -3,5 +3,6 @@ import { bindable, bindingMode } from 'aurelia-framework';
 import { InputControl } from '@leanup/form';
 
 export class AppInput {
-  @bindable({ defaultBindingMode: bindingMode.twoWay }) public control!: InputControl;
+  @bindable({ defaultBindingMode: bindingMode.twoWay })
+  public control!: InputControl;
 }
--- packages/cli/frameworks/aurelia/template/src/components/series/editor/component.aurelia.ts
+++ packages/cli/frameworks/aurelia/template/src/components/series/editor/component.aurelia.ts
@@ -4,5 +4,6 @@ import { EditorSerieController } from './controller';
 import { EditorForm } from './editor.form';
 
 export class EditorSerie extends EditorSerieController {
-  @bindable({ defaultBindingMode: bindingMode.twoWay }) public editorForm!: EditorForm;
+  @bindable({ defaultBindingMode: bindingMode.twoWay })
+  public editorForm!: EditorForm;
 }
