da67d5831da41156d201547a648bfb7b6ec99d5e
Martin Oppitz <github@martinoppitz.com>
2022-12-23 06:56:25 +0100

    refactor: v2

--- packages/create-leanup/src/cli.ts
+++ packages/create-leanup/src/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { create } from 'create-create-app';
+import { create } from 'create-create-cli';
 import { resolve } from 'path';
 
 const templateRoot = resolve(__dirname, '..', 'templates');
